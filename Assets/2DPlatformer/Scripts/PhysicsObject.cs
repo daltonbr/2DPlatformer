71cd15d63d3b0d85e55dce20b0d0ab62f8707563
Dalton Lima <daltonvarussa@gmail.com>
2018-09-15 00:50:50 -0300

    Detecting overlaps and scripting collision

--- Assets/2DPlatformer/Scripts/PhysicsObject.cs
+++ Assets/2DPlatformer/Scripts/PhysicsObject.cs
@@ -9,9 +9,17 @@ using UnityEngine.Assertions;
 /// </summary>
 public class PhysicsObject : MonoBehaviour
 {
+    public float minGroundNormalY = 0.65f;
     public float gravityModifier = 1f;
+    protected bool grounded;
+    protected Vector2 groundNormal;
     protected Rigidbody2D rb2d;
     protected Vector2 velocity;
+    protected ContactFilter2D contactFilter;
+    protected RaycastHit2D[] hitBuffer = new RaycastHit2D[16];
+    protected List<RaycastHit2D> hitBufferList = new List<RaycastHit2D> (16);
+    protected const float minMoveDistance = 0.001f;
+    protected const float shellRadius = 0.01f;
 
     void OnEnable()
     {
@@ -19,16 +27,63 @@ public class PhysicsObject : MonoBehaviour
         Assert.IsNotNull(rb2d);
     }
 
+    void Start()
+    {
+        // not checking against Triggers
+        contactFilter.useTriggers = false;
+        // Use the usual Physics 2D layer collision matrix (Player Prefs -> Physics 2D)
+        contactFilter.SetLayerMask(Physics2D.GetLayerCollisionMask(gameObject.layer));
+    }
+
     void FixedUpdate()
     {
         velocity += gravityModifier * Physics2D.gravity * Time.deltaTime;
+        grounded = false;
         Vector2 deltaPosition = velocity * Time.deltaTime;
         Vector2 move = Vector2.up * deltaPosition.y;
-        Movement(move);
+        Movement(move, true);
     }
 
-    void Movement(Vector2 move)
+    void Movement(Vector2 move, bool yMovement)
     {
-        rb2d.position = rb2d.position + move;
+        float distance = move.magnitude;
+
+        if (distance > minMoveDistance)
+        {
+            int count = rb2d.Cast(move, contactFilter, hitBuffer, distance + shellRadius);
+            hitBufferList.Clear();
+            for (int i = 0; i < count; i++)
+            {
+                hitBufferList.Add(hitBuffer[i]);
+            }
+
+            for (int i = 0; i < hitBufferList.Count; i++)
+            {
+                Vector2 currentNormal = hitBufferList[i].normal;
+                if (currentNormal.y > minGroundNormalY)
+                {
+                    grounded = true;
+                    if (yMovement)
+                    {
+                        groundNormal = currentNormal;
+                        currentNormal.x = 0;
+                    }
+                }
+
+                // When hitting a ceilling we are cancelling out the part of the velocity that will be stopped by the collision
+                // so we can keep some horizontal movement
+                float projection = Vector2.Dot(velocity, currentNormal);
+                if (projection < 0)
+                {
+                    velocity = velocity - projection * currentNormal;
+                }
+
+                // Prevent us to get stuck inside other colliders
+                float modifiedDistance = hitBufferList[i].distance - shellRadius;
+                distance = modifiedDistance < distance ? modifiedDistance : distance;
+            }
+        }
+
+        rb2d.position = rb2d.position + move.normalized * distance;
     }
 }
