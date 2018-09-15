18b454cc6d199a5ad14b08c68fa60053e0d978a3
Dalton Lima <daltonvarussa@gmail.com>
2018-09-15 01:58:18 -0300

    adding Player Animation

--- Assets/2DPlatformer/Scripts/PlayerPlatformerController.cs
+++ Assets/2DPlatformer/Scripts/PlayerPlatformerController.cs
@@ -1,12 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Assertions;
 
+[RequireComponent(typeof(SpriteRenderer))]
+[RequireComponent(typeof(Animator))]
+/// <summary> Old-school 2D Player Controller </summary>
 public class PlayerPlatformerController : PhysicsObject
 {
 
     public float maxSpeed = 7;
     public float jumpTakeOffSpeed = 7;
+
+    private SpriteRenderer spriteRenderer;
+    private Animator animator;
+
+    void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+        Assert.IsNotNull(spriteRenderer);
+        Assert.IsNotNull(animator);
+    }
 
     protected override void ComputeVelocity()
     {
@@ -25,6 +40,17 @@ public class PlayerPlatformerController : PhysicsObject
                 velocity.y = velocity.y * 0.5f;
             }
         }
+
+        // Player facing forward
+        bool flipSprite = (spriteRenderer.flipX ? (move.x > 0.01f) : (move.x < 0.01f));
+        if (flipSprite)
+        {
+            spriteRenderer.flipX = !spriteRenderer.flipX;
+        }
+
+        animator.SetBool("grounded", grounded);
+        animator.SetFloat("velocityX", Mathf.Abs(velocity.x) / maxSpeed);
+
         targetVelocity = move * maxSpeed;
     }
 }
